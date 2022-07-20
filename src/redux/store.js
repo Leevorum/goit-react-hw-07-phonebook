63592a288359c0b223cdd3e429d197ed40a07c22
Leevorum <di4111ua@gmail.com>
2022-07-20 17:26:33 +0300

    devtoolls hide

--- src/redux/store.js
+++ src/redux/store.js
@@ -11,6 +11,7 @@ const store = configureStore({
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware().concat(contactsApi.middleware),
+  devTools: process.env.NODE_ENV === 'development',
 });
 
 export { store };
